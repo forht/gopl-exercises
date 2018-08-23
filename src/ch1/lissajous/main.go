a326ae94ca797996bbbfdb513d4ca014d99d8e32
Daniele Parisi <daniele@parisi.xyz>
2018-08-23 09:26:48 +0200

    url parameters

---
+++ src/ch1/lissajous/main.go
@@ -0,0 +1,95 @@
+// Exercise 1.12
+
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"math"
+	"math/rand"
+	"os"
+	"strconv"
+)
+
+//!-main
+// Packages not needed by version in book.
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+//!+main
+
+var palette = []color.Color{color.Black, color.RGBA{0, 0xff, 0, 0xff},
+	color.RGBA{0xff, 0, 0, 0xff}}
+
+const (
+	blackIndex = iota // first color in palette
+	greenIndex        // next color in palette
+	redIndex
+)
+
+func main() {
+	//!-main
+	// The sequence of images is deterministic unless we seed
+	// the pseudo-random number generator using the current time.
+	// Thanks to Randall McPherson for pointing out the omission.
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	cycles := 5
+	if len(os.Args) > 1 && os.Args[1] == "web" {
+		//!+http
+		handler := func(w http.ResponseWriter, r *http.Request) {
+			if err := r.ParseForm(); err == nil {
+				if fcycles, found := r.Form["cycles"]; found || len(fcycles) != 0 {
+					if c, err := strconv.Atoi(fcycles[0]); err == nil {
+						cycles = c
+					}
+				}
+			}
+			lissajous(os.Stdout, cycles)
+		}
+
+		http.HandleFunc("/", handler)
+		//!-http
+		log.Fatal(http.ListenAndServe("localhost:8000", nil))
+		return
+	}
+	//!+main
+	lissajous(os.Stdout, cycles)
+}
+
+func lissajous(out io.Writer, cycles int) {
+	const (
+		res     = 0.001 // angular resolution
+		size    = 100   // image canvas covers [-size..+size]
+		nframes = 64    // number of animation frames
+		delay   = 8     // delay between frames in 10ms units
+	)
+	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0 // phase difference
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < float64(cycles)*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			var idx uint8
+			if idx = 1; t > float64(cycles)*math.Pi {
+				idx = 2
+			}
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
+				idx)
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim) // NOTE: ignoring encoding errors
+}
+
+//!-main
